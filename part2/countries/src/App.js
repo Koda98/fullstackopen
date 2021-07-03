d688f858d6b4d3d8104326e8074dd8f3c708292a
Kyle Oda <1624udon@gmail.com>
2021-07-02 19:28:05 -0700

    finished exercise 2.12

---
+++ part2/countries/src/App.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios"
+
+const Query = ({ eventHandler }) => (
+    <div>
+        find countries<input onChange={eventHandler} />
+    </div>
+)
+
+const Country = ({ country }) => (
+    <div>
+        <h1>{country.name}</h1>
+        <div>capital {country.capital}</div>
+        <div>population {country.population}</div>
+
+        <h2>languages</h2>
+        <ul>
+            {country.languages.map(language =>
+                <li key={language.name}>{language.name}</li>
+            )}
+        </ul>
+        <img 
+            src={country.flag}
+            alt={`flag of ${country.name}`}
+            width="100"
+            />
+    </div>
+)
+
+const Countries = ({ countries }) => {
+    if (countries.length > 10) {
+        return (
+            <div>
+                Too many matches, specify another filter
+            </div>
+        )
+    }
+    else if (countries.length === 1) {
+        return (
+            <Country 
+                key={countries[0]}
+                country={countries[0]} 
+            />
+        )
+    }
+    return (
+        <div>
+            {countries.map(country =>
+                <div key={country.name}>{country.name}</div>
+            )}
+        </div>
+    )
+}
+
+function App() {
+    const [query, setQuery] = useState('')
+    const [countries, setCountries] = useState([])
+
+    useEffect(() => {
+        console.log("Effect")
+        axios
+            .get("https://restcountries.eu/rest/v2/all")
+            .then(response => {
+                console.log("Promise fulfilled")
+                setCountries(response.data)
+            })
+    }, [])
+
+    const handleQuery = (event) => {
+        console.log("handleSearch value: ", event.target.value)
+        setQuery(event.target.value)
+    }
+
+    const countriesToShow = countries.filter(country =>
+        country.name.toLowerCase().includes(query.toLowerCase())
+    )
+
+    return (
+        <div>
+            <Query eventHandler={handleQuery} />
+            <Countries countries={countriesToShow} />
+        </div>
+    );
+}
+
+export default App;

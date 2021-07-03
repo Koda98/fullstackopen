68b625bcde630d00cdd341bd778df7c050897300
Kyle Oda <1624udon@gmail.com>
2021-07-03 14:43:06 -0700

    finished exercises 2.19 and 2.20, part 2 complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,12 +3,15 @@ import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import phonebookService from './services/phonebook'
+import Notification from './components/Notification'
 
 const App = () => {
     const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [filter, setFilter] = useState('')
+    const [notificationMessage, setNotificationMessage] = useState(null)
+    const [notificationError, setNotificationError] = useState(false)
 
     useEffect(() => {
         phonebookService
@@ -27,20 +30,42 @@ const App = () => {
         const person = persons.find(p => p.name === newName)
         if (person) {
             if (window.confirm(`${newName} is already added to the phonebook, replace the old number with a new one?`)) {
-                const changedPerson = {...person, number: newNumber}
+                const changedPerson = { ...person, number: newNumber }
                 phonebookService
                     .update(person.id, changedPerson)
                     .then(returnedPerson => {
+                        setNotificationMessage(`updated ${person.name}`)
+                        setNotificationError(false)
+                        setTimeout(() => {
+                            setNotificationMessage(null)
+                            setNotificationError(null)
+                        }, 5000)
                         setPersons(persons.map(p => p.id !== person.id ? p : returnedPerson))
                         console.log(`Updated ${person.name}`)
                     })
+                    .catch(error => {
+                        setNotificationMessage(
+                            `Information of ${person.name} has alredy been removed from the server`
+                        )
+                        setNotificationError(true)
+                        setTimeout(() => {
+                            setNotificationMessage(null)
+                            setNotificationError(null)
+                        }, 5000)
+                        setPersons(persons.filter(p => p.id !== person.id))
+                    })
             }
-
         }
         else {
             phonebookService
                 .create(personsObject)
                 .then(returnedPerson => {
+                    setNotificationMessage(`Added ${newName}`)
+                    setNotificationError(false)
+                    setTimeout(() => {
+                        setNotificationMessage(null)
+                        setNotificationError(null)
+                    }, 5000)
                     setPersons(persons.concat(returnedPerson))
                     setNewName('')
                     setNewNumber('')
@@ -68,38 +93,45 @@ const App = () => {
     const deletePerson = person => {
         if (window.confirm(`Delete ${person.name} ?`)) {
             phonebookService
-            .deletePerson(person.id)
-            .then(response => {
-                setPersons(persons.filter(p => p.id !== person.id))
-                console.log(`Deleted ${person.name} with id ${person.id}`)
-            })
+                .deletePerson(person.id)
+                .then(response => {
+                    setNotificationMessage(`Deleted ${person.name}`)
+                    setNotificationError(false)
+                    setTimeout(() => {
+                        setNotificationMessage(null)
+                        setNotificationError(null)
+                    }, 5000)
+                    setPersons(persons.filter(p => p.id !== person.id))
+                    console.log(`Deleted ${person.name} with id ${person.id}`)
+                })
         }
     }
 
 
-const personsToShow = persons.filter(person =>
-    person.name.toUpperCase().includes(filter.toUpperCase())
-)
+    const personsToShow = persons.filter(person =>
+        person.name.toUpperCase().includes(filter.toUpperCase())
+    )
 
-return (
-    <div>
-        <h2>Phonebook</h2>
-        <Filter eventHandler={handleFilter} />
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <Notification message={notificationMessage} isError={notificationError} />
+            <Filter eventHandler={handleFilter} />
 
-        <h3>add a new</h3>
-        <PersonForm
-            addPerson={addPerson}
-            handleAddName={handleAddName}
-            handleAddNumber={handleAddNumber}
-        />
+            <h3>add a new</h3>
+            <PersonForm
+                addPerson={addPerson}
+                handleAddName={handleAddName}
+                handleAddNumber={handleAddNumber}
+            />
 
-        <h3>Numbers</h3>
-        <Persons 
-            persons_array={personsToShow}
-            deletePerson={deletePerson}
-        />
-    </div>
-)
+            <h3>Numbers</h3>
+            <Persons
+                persons_array={personsToShow}
+                deletePerson={deletePerson}
+            />
+        </div>
+    )
 }
 
 export default App
